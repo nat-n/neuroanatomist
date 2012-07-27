b55d47a4ea6c0c64f029ba5e0af2f081d889776c
Nat Noordanus <n@natn.me>
2012-07-27 15:47:01 +0100

    added validation so there can be only one root "Thing" type

--- app/models/type.rb
+++ app/models/type.rb
@@ -6,5 +6,15 @@ class Type < ActiveRecord::Base
   has_many    :object_relations, :class_name => 'Relation', :foreign_key => 'object_type_id', :dependent => :destroy
   validates_uniqueness_of :name
   validates_presence_of :name, :supertype
+  validate :not_being_set_as_root
   
+  
+  
+  private
+    def not_being_set_as_root
+      errors.add(:supertype, "Only the root type called 'Thing' may be it's own supertype!") if supertype == self and not self.name == "Thing"
+    end
+    
 end
+
+
