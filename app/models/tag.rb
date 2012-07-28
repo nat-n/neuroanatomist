7938a14a5ede13a4009f1ca2009748b63fe8d4e3
Nat Noordanus <n@natn.me>
2012-07-28 17:52:15 +0100

    resources can select resource type and either select or create tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,5 +2,16 @@ class Tag < ActiveRecord::Base
   belongs_to :thing
   has_and_belongs_to_many :resource
   has_many :ratings
+  validates_presence_of :name
+  validates_uniqueness_of :name
+  
+  
+  def self.find_by_name tag_name
+    Tag.where(:name => tag_name).first
+  end
+  
+  def self.find_or_create tag_name
+    Tag.find_by_name(tag_name) or Tag.create(:name => tag_name)
+  end
   
 end
