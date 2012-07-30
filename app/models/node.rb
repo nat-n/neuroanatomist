997539658a102f6a65702091a287fdc5a6cba21c
Nat Noordanus <n@natn.me>
2012-07-30 14:22:41 +0100

    setup refactored node

--- app/models/node.rb
+++ app/models/node.rb
@@ -4,5 +4,17 @@ class Node < ActiveRecord::Base
   has_many  :subtopics, :class_name => 'Section', :foreign_key => 'topic_id', :dependent => :destroy
   has_one   :bibliography, :as => :referencable
   has_one   :perspective
+  has_one   :tag
+  validates_uniqueness_of :name
+  validates_presence_of :name
+  
+  
+  def self.find_by_name node_name
+    Node.where(:name => node_name).first
+  end
+  
+  def self.find_or_create node_name
+    Node.find_by_name(node_name) or Node.create(:name => node_name)
+  end
   
 end
