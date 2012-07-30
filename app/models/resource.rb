a50d4ea24136ef969af0733facf3ac935e103cd9
Nat Noordanus <n@natn.me>
2012-07-30 14:21:44 +0100

    fixed typo

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,7 +1,12 @@
 class Resource < ActiveRecord::Base
-  belongs_to :resources_type
+  belongs_to :resource_type
   has_and_belongs_to_many :bibliographies
   has_and_belongs_to_many :tags
   has_many :ratings, :as => :taggable
+  validates_presence_of :resource_type, :title
+  
+  def type
+    :resource_type
+  end
   
 end
