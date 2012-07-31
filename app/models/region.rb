a45d9310dd09c370906c8fa6e7b84d2184b2628c
Nat Noordanus <n@natn.me>
2012-07-31 14:26:19 +0100

    link regions back to perspectives

--- app/models/region.rb
+++ app/models/region.rb
@@ -3,6 +3,8 @@ class Region < ActiveRecord::Base
   has_and_belongs_to_many :region_sets
   has_many :region_definitions
   has_many :shape_sets, :through => :region_definitions
+  has_many :region_styles
+  has_many :perspectives, :through => :region_styles
   
   alias :definitions :region_definitions
     
--- app/models/region_set.rb
+++ app/models/region_set.rb
@@ -1,5 +1,4 @@
 class RegionSet < ActiveRecord::Base
   has_and_belongs_to_many :regions
-  has_many :perspectives
   
 end
