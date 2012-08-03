88fc974780e7ba24d13009e0c023c45ef46c4075
Nat Noordanus <n@natn.me>
2012-08-03 17:06:57 +0100

    decompositions are automatically ranked/sorted and includes in relevant views

--- app/models/region.rb
+++ app/models/region.rb
@@ -11,6 +11,14 @@ class Region < ActiveRecord::Base
   
   alias :definitions :region_definitions
   
+  def decompositions
+    # position any unranked decompositions at end of ranking preserving order
+    decompositions = Decomposition.where("region_id = ?", id)
+    max_rank = (decompositions.map(&:rank).compact.max or 0)
+    decompositions = decompositions.select{|d| d[:rank]}.sort + 
+                     decompositions.select{|d| !d[:rank]}.map{|d| d.rank = (max_rank+=1); d }
+  end
+  
   def definition_for shape_set
     (definitions = region_definitions.select{ |x| x.shape_set_id == shape_set.id }).empty? ? false : definitions[0]
   end
