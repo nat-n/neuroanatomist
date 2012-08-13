d8ecf2e924bbecb88a0dcba701c3a7d1b6d7e6a6
Nat Noordanus <n@natn.me>
2012-08-13 20:30:12 +0100

    streamlined migrations for perspectives

--- db/migrate/20120730163423_describe_perspectives.rb
+++
@@ -1,9 +0,0 @@
-class DescribePerspectives < ActiveRecord::Migration
-  def up
-    add_column :perspectives, :description, :string
-  end
-
-  def down
-    remove_column :perspectives, :description
-  end
-end
--- db/migrate/20120731121850_perspectives_hierarchy.rb
+++
@@ -1,9 +0,0 @@
-class PerspectivesHierarchy < ActiveRecord::Migration
-  def up
-    add_column :perspectives, :style_set_id, :integer
-  end
-
-  def down
-    remove_column :perspectives, :style_set_id
-  end
-end
