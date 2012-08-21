f277fd639c74900658db4555ddfeb135e1a03ff7
Nat Noordanus <n@natn.me>
2012-08-22 00:39:14 +0100

    added alias, name and admin fields

--- db/migrate/20120821211009_devise_create_users.rb
+++ db/migrate/20120821211009_devise_create_users.rb
@@ -10,11 +10,15 @@ class DeviseCreateUsers < ActiveRecord::Migration
       # t.confirmable
       # t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
       # t.token_authenticatable
-
-
+      
+      t.string :alias
+      t.string :name
+      t.boolean :admin, :default => false
+      
       t.timestamps
     end
 
+    add_index :users, :alias,                :unique => true
     add_index :users, :email,                :unique => true
     add_index :users, :reset_password_token, :unique => true
     # add_index :users, :confirmation_token,   :unique => true
--- db/schema.rb
+++ db/schema.rb
@@ -305,8 +305,8 @@ ActiveRecord::Schema.define(:version => 20120821211009) do
   add_index "types", ["supertype_id"], :name => "index_types_on_supertype_id"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "email",                                 :default => "",    :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -315,10 +315,14 @@ ActiveRecord::Schema.define(:version => 20120821211009) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                            :null => false
-    t.datetime "updated_at",                                            :null => false
+    t.string   "alias"
+    t.string   "name"
+    t.boolean  "admin",                                 :default => false
+    t.datetime "created_at",                                               :null => false
+    t.datetime "updated_at",                                               :null => false
   end
 
+  add_index "users", ["alias"], :name => "index_users_on_alias", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
