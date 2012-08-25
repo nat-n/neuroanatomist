6ce5c1af38a8313fcf614060e24a32a9c856fd29
Nat Noordanus <n@natn.me>
2012-08-25 19:08:48 +0100

    added ontology navigation to the main layout and trigger for it in the ontology_base controller

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,16 +1,13 @@
 class Admin::BaseController < ApplicationController
   before_filter :authorize_admin!
-  before_filter :use_admin_layout
-  
-  
-  #layout 'admin_areas'
+  before_filter :enable_admin_layout
   
   def index
     
   end
   
   private
-    def use_admin_layout
+    def enable_admin_layout
       @admin_layout = true
     end
 end
