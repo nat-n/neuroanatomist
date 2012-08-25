630154ecdf466168f47530136a7c55d5ae5b45c4
Nat Noordanus <n@natn.me>
2012-08-25 18:55:40 +0100

    switched to using a view variable for admin layout rather than a separate layout

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,9 +1,16 @@
 class Admin::BaseController < ApplicationController
   before_filter :authorize_admin!
+  before_filter :use_admin_layout
   
-  layout 'admin_areas'
+  
+  #layout 'admin_areas'
   
   def index
     
   end
+  
+  private
+    def use_admin_layout
+      @admin_layout = true
+    end
 end
