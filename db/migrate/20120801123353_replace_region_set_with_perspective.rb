01f422baa9510743b2a4ddfe10eb3430fee95db8
Nat Noordanus <n@natn.me>
2012-09-11 18:49:54 +0100

    fixing up migrations etc

--- db/migrate/20120801123353_replace_region_set_with_perspective.rb
+++ db/migrate/20120801123353_replace_region_set_with_perspective.rb
@@ -2,7 +2,5 @@ class ReplaceRegionSetWithPerspective < ActiveRecord::Migration
   def change
     add_column    :shape_sets, :default_perspective_id, :integer
     add_column    :perspectives, :default_for_shape_set_id, :integer
-    
-    remove_column :shape_sets, :default_region_set_id
   end
 end
